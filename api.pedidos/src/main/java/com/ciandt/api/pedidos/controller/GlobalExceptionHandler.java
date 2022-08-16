e76655db5aba3366f7231cdb0192c9a4e7e3c690
rebsviana <rebecav@ciandt.com>
2022-08-16 11:21:16 -0300

    [api.pedidos]: add Exception Handler PedidoNotFoundException and PedidoJaCadastrado

--- api.pedidos/src/main/java/com/ciandt/api/pedidos/controller/GlobalExceptionHandler.java
+++ api.pedidos/src/main/java/com/ciandt/api/pedidos/controller/GlobalExceptionHandler.java
@@ -1,14 +1,14 @@
 package com.ciandt.api.pedidos.controller;
 
 import com.ciandt.api.pedidos.dto.ErrorDto;
+import com.ciandt.api.pedidos.exception.PedidoJaCadastrado;
+import com.ciandt.api.pedidos.exception.PedidoNotFoundException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import javax.validation.ConstraintViolationException;
 
 @Log4j2
 @ControllerAdvice
@@ -21,6 +21,28 @@ public class GlobalExceptionHandler {
                 ErrorDto.builder()
                         .code(HttpStatus.BAD_REQUEST.value())
                         .message(exception.getFieldError().getDefaultMessage())
+                        .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDto);
+    }
+
+    @ExceptionHandler(value = PedidoNotFoundException.class)
+    public ResponseEntity<ErrorDto> handlePedidoNotFoundException(PedidoNotFoundException exception){
+        log.error("Pedido Not Found Exception: {}", exception.getMessage());
+        var errorDto =
+                ErrorDto.builder()
+                        .code(HttpStatus.NOT_FOUND.value())
+                        .message(exception.getMessage())
+                        .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDto);
+    }
+
+    @ExceptionHandler(value = PedidoJaCadastrado.class)
+    public ResponseEntity<ErrorDto> handleValidationException(PedidoJaCadastrado exception){
+        log.error("Validation Exception: {}", exception.getMessage());
+        var errorDto =
+                ErrorDto.builder()
+                        .code(HttpStatus.BAD_REQUEST.value())
+                        .message(exception.getMessage())
                         .build();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDto);
     }
