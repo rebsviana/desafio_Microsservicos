2fc87ecb12a44db6a86759d40bdc87bb11a588fe
rebsviana <rebecav@ciandt.com>
2022-08-16 14:29:34 -0300

    [api.pagamento]: fix validation PagamentoDto

--- api.pagamento/src/main/java/com/ciandt/api/pagamento/dto/PagamentoDto.java
+++ api.pagamento/src/main/java/com/ciandt/api/pagamento/dto/PagamentoDto.java
@@ -2,9 +2,11 @@ package com.ciandt.api.pagamento.dto;
 
 import com.ciandt.api.pagamento.enums.FormaDePagamento;
 import com.ciandt.api.pagamento.enums.Status;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -15,7 +17,8 @@ import java.math.BigDecimal;
 @Data
 public class PagamentoDto {
 
-    @ApiModelProperty(example = "1")
+    @ApiModelProperty(required = false)
+    @JsonIgnore
     private Long id;
 
     @NotNull (message = "O valor nao pode ser vazio")
@@ -24,20 +27,20 @@ public class PagamentoDto {
     private BigDecimal valor;
 
     @NotNull (message = "O nome não pode ser vazio")
-    @Max(value = 100, message = "O nome deve ter no máximo 100 caracteres")
-//    @ApiModelProperty(example = "Maria", required = true)
+    @Length(max = 100, message = "O nome deve ter no máximo 100 caracteres")
+    @ApiModelProperty(example = "Maria", required = true)
     private String nome;
 
     @NotBlank (message = "O número não pode ser vazio")
-    @Max(value = 100, message = "O numero deve ter no máximo 100 caracteres")
-//    @ApiModelProperty(example = "124578", required = true)
+    @Length(max = 100, message = "O numero deve ter no máximo 100 caracteres")
+    @ApiModelProperty(example = "124578", required = true)
     private String numero;
 
     @ApiModelProperty(example = "08/09/2022")
     private String expiracao;
 
     @NotBlank (message = "O código não pode ser vazio")
-    @Size(min = 3, max = 3, message = "O código deve ter 3 caracteres")
+    @Length(min = 3, max = 3, message = "O código deve ter 3 caracteres")
     @ApiModelProperty(example = "abc", required = true)
     private String codigo;
 
