45b1f82bd364fa0dd997cf6400918013f3750968
rebsviana <rebecav@ciandt.com>
2022-08-16 11:23:19 -0300

    [api.pagamento]: add Exception Handler PagamentoNotFoundException

--- api.pagamento/src/main/java/com/ciandt/api/pagamento/controller/GlobalExceptionHandler.java
+++ api.pagamento/src/main/java/com/ciandt/api/pagamento/controller/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.ciandt.api.pagamento.controller;
 
 import com.ciandt.api.pagamento.dto.ErrorDto;
+import com.ciandt.api.pagamento.exception.PagamentoNotFoundException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +11,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Log4j2
 @ControllerAdvice
-public class GlobalExceptionHandler{
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorDto> handleValidationException(MethodArgumentNotValidException exception){
+    public ResponseEntity<ErrorDto> handleValidationException(MethodArgumentNotValidException exception) {
         log.error("Validation Exception: {}", exception.getMessage());
         var errorDto =
                 ErrorDto.builder()
@@ -23,26 +24,25 @@ public class GlobalExceptionHandler{
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDto);
     }
 
+    @ExceptionHandler(value = PagamentoNotFoundException.class)
+    public ResponseEntity<ErrorDto> handlePedidoNotFoundException(PagamentoNotFoundException exception){
+        log.error("Pagamento Not Found Exception: {}", exception.getMessage());
+        var errorDto =
+                ErrorDto.builder()
+                        .code(HttpStatus.NOT_FOUND.value())
+                        .message(exception.getMessage())
+                        .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDto);
+    }
 
-//    @ExceptionHandler(value = Exception.class)
-//    public ResponseEntity<ErrorDto> handleValidationException(Exception exception){
-//        log.error("Error Exception: {}", exception.getMessage());
-//        var errorDto =
-//                ErrorDto.builder()
-//                        .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-//                        .message(exception.getMessage())
-//                        .build();
-//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDto);
-//    }
-
-//    @ExceptionHandler(value = DataIntegrityViolationException.class)
-//    public ResponseEntity<ErrorDto> handleValidationException(DataIntegrityViolationException exception){
-//        log.error("DataIntegrityViolationException: {}", exception.getMessage());
-//        var errorDto =
-//                ErrorDto.builder()
-//                        .code(HttpStatus.BAD_REQUEST.value())
-//                        .message(exception.getMessage())
-//                        .build();
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDto);
-//    }
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ErrorDto> handleException(Exception exception) {
+        log.error("Error Exception: {}", exception.getMessage());
+        var errorDto =
+                ErrorDto.builder()
+                        .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                        .message(exception.getMessage())
+                        .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDto);
+    }
 }
