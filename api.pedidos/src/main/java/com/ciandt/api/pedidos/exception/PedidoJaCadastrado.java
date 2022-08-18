3318abd4b34b86bc2ea8c7167c6446a84d944dcb
rebsviana <rebecav@ciandt.com>
2022-08-18 17:06:21 -0300

    [api.pedidos]: fix extends exception

--- api.pedidos/src/main/java/com/ciandt/api/pedidos/exception/PedidoJaCadastrado.java
+++ api.pedidos/src/main/java/com/ciandt/api/pedidos/exception/PedidoJaCadastrado.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class PedidoJaCadastrado extends Exception {
+public class PedidoJaCadastrado extends RuntimeException {
 
     public static final String MSG = "Pedido ja cadastrado";
     public PedidoJaCadastrado() {
